2bf34e5b2ed9ea6e083a9103caadaf1ff348934b
Ling Lung <ling.lung@natgeo.com>
2020-10-18 23:26:57 -0500

    Added Signup, Login and logout

---
+++ mrrobot_scavenger_hunt/apps/game/views.py
@@ -0,0 +1,62 @@
+from django.contrib.auth.decorators import login_required
+from django.http import HttpResponse
+from django.shortcuts import redirect
+from django.template import loader
+
+from django.contrib.auth import login, authenticate
+from django.contrib.auth import logout as log_out
+from django.contrib.auth.models import User
+
+
+def index(request):
+    username = request.POST.get('username')
+    password = request.POST.get('password')
+    user = authenticate(request, username=username, password=password)
+    context = {}
+    print(user)
+    if user is not None:
+        login(request, user)
+        context.update({
+            'game_in_progress': False,
+        })
+    else:
+        context.update({
+            'message': 'invalid_credentials. Please try again',
+        })
+
+    template = loader.get_template('index.html')
+
+    return HttpResponse(template.render(context, request))
+
+
+def signup(request):
+    username = request.POST.get('username')
+    password = request.POST.get('password')
+    context = {
+        'message': 'Please enter your information',
+    }
+    try:
+        User.objects.get(username=username)
+        context['message'] = "This username already exists"
+    except User.DoesNotExist:
+        if username and password:
+            user = User.objects.create(username=username)
+            user.set_password(password)
+            user.save()
+            return redirect('index')
+        else:
+            context['message'] = "Please fill all the fields"
+
+    template = loader.get_template('signup.html')
+
+    return HttpResponse(template.render(context, request))
+
+
+def logout(request):
+    log_out(request)
+    template = loader.get_template('logout.html')
+    return HttpResponse(template.render({}, request))
+
+@login_required
+def game(request):
+    pass
--- mrrobot_scavenger_hunt/settings.py
+++ mrrobot_scavenger_hunt/settings.py
@@ -13,6 +13,8 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
+import os
+
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
@@ -54,7 +56,7 @@ ROOT_URLCONF = 'mrrobot_scavenger_hunt.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
