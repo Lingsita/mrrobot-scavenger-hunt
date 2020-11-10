46a62a046dd2772b3636ebf762e7090c210b1cb2
Liliana Loaiza P <liliana.loaiza@globant.com>
2020-11-10 01:57:28 -0500

    Lowercase username in sign-in form (due to phone usability)

--- mrrobot_scavenger_hunt/apps/game/views.py
+++ mrrobot_scavenger_hunt/apps/game/views.py
@@ -23,7 +23,7 @@ def index(request):
     if request.method == 'POST':
         username = request.POST.get('username')
         password = request.POST.get('password')
-        user = authenticate(request, username=username, password=password)
+        user = authenticate(request, username=username.lower(), password=password)
         if user is not None:
             login(request, user)
             context.update({
